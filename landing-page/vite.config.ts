8ae99a7ddf3960c861a29cb691d11aec62991a76
Vinodh-Shekhar <vinvictorious92@gmail.com>
2026-03-26 13:20:49 +0530

    Update vite.config.ts

--- landing-page/vite.config.ts
+++ landing-page/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FrameBench/', // 👈 THIS IS THE FIX
+  base: '/', // ✅ correct for Vercel
 })
